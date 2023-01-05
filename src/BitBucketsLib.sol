567b3a7157eae71c39d1898ccd455808bd8519df
Quentin Garchery <garchery.quentin@gmail.com>
2023-01-05 12:19:55 +0100

    docs: add NatSpec comments & regular comments

--- src/BitBucketsLib.sol
+++ src/BitBucketsLib.sol
@@ -5,15 +5,20 @@ import "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import "./libraries/BitTwiddling.sol";
 import "./BucketLib.sol";
 
+/// @title Bit buckets.
+/// @author Morpho Labs.
+/// @custom:contact security@morpho.xyz
+/// @notice Buckets data-structure using bit tricks to achieve constant complexity.
 library BitBucketsLib {
     using BitTwiddling for uint256;
     using BitTwiddling for bytes32;
     using BucketLib for BucketLib.Bucket;
 
+    // Buckets are indexed by bit masks for efficiency. Valid bit masks 'byte addressed', meaning that valid masks are of the form '0xff << n' where n is a multiple of 8.
     struct BitBuckets {
-        mapping(bytes32 => BucketLib.Bucket) buckets;
-        mapping(address => bytes32) maskOf;
-        bytes32 bucketsMask; // bitmask for all used buckets
+        mapping(bytes32 => BucketLib.Bucket) buckets; // All the buckets indexed by their corresponding mask.
+        mapping(address => bytes32) maskOf; // The mask of a given account.
+        bytes32 bucketsMask; // The disjunction of the masks of all the buckets used.
     }
 
     /// ERRORS ///
@@ -24,6 +29,9 @@ library BitBucketsLib {
     /// INTERNAL ///
 
     /// @notice Updates an account in the `_bitBuckets`.
+    /// @param _bitBuckets The bit buckets to modify.
+    /// @param _id The address of the account to update.
+    /// @param _newValue The new value of the account to update.
     function update(
         BitBuckets storage _bitBuckets,
         address _id,
@@ -50,6 +58,10 @@ library BitBucketsLib {
         }
     }
 
+    /// @notice Returns the value of the account linked to `_id`.
+    /// @param _bitBuckets The bit buckets to search in.
+    /// @param _id The address of the account.
+    /// @return The value of the account.
     function getValueOf(BitBuckets storage _bitBuckets, address _id)
         internal
         view
@@ -59,6 +71,10 @@ library BitBucketsLib {
         return _bitBuckets.buckets[formerMask].getValueOf(_id);
     }
 
+    /// @notice Returns an address with value matching the input value.
+    /// @param _bitBuckets The bit buckets to search in.
+    /// @param _value The input value.
+    /// @return The address of the with matching value.
     function getHead(BitBuckets storage _bitBuckets, uint96 _value)
         internal
         view
@@ -78,6 +94,11 @@ library BitBucketsLib {
 
     /// PRIVATE ///
 
+    /// @notice Removes an account in the `_bitBuckets`.
+    /// @dev Only call when this function when `_id` is in the `_bitBuckets` with mask `_formerMask`.
+    /// @param _bitBuckets The bit buckets in which to remove the account.
+    /// @param _formerMask The mask of the account to remove.
+    /// @param _id The address of the account to remove.
     function _remove(
         BitBuckets storage _bitBuckets,
         bytes32 _formerMask,
@@ -89,6 +110,12 @@ library BitBucketsLib {
         if (formerBucket.getLength() == 0) _bitBuckets.bucketsMask &= ~_formerMask;
     }
 
+    /// @notice Adds an account in the `_bitBuckets`.
+    /// @dev Only call when this function when `_id` is not in the `_bitBuckets` and when the mask `_newMask` corresponds to value `_newValue`.
+    /// @param _bitBuckets The bit buckets in which to add the account.
+    /// @param _newMask The mask of the account to insert.
+    /// @param _id The address of the account to insert.
+    /// @param _newValue The value of the account to insert.
     function _insert(
         BitBuckets storage _bitBuckets,
         bytes32 _newMask,
